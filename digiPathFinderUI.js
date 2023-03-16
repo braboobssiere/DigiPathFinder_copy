270c79841cf84102883ab85b04f04a3271a748f0
Boobs <86918103+braboobssiere@users.noreply.github.com>
2023-03-16 22:11:59 +0700

    Update digiPathFinderUI.js

--- digiPathFinderUI.js
+++ digiPathFinderUI.js
@@ -33,7 +33,7 @@ function showRoute(route){
 			
 			pathContent+="<img class='flex-item' src='"+"images/"+route[i]+".png' />";
 			pathContent+="<div data-digimonid='"+route[i]+"' class='flex-item digi_name digi_name_placeholder'>";
-			//pathContent+=pathFinder.digiData[route[i]].name;
+			pathContent+=pathFinder.digiData[route[i]].id;
 			pathContent+="</div>";
 			pathContent+="<div data-target='"+route[i]+"' class='db_link flex-item'>";
 			pathContent+="<i class='fa fa-external-link' aria-hidden='true'></i>";
